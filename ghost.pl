68c6d263e32fdcc47f98e0cb1048c613b2ebd4ea
Stefano <stefano.forti92@gmail.com>
2024-10-03 22:14:11 +0200

    added (ghost) file with heuristic inside Prolog

---
+++ ghost.pl
@@ -0,0 +1,134 @@
+%# Include di file ausiliari.
+:- consult('resources/variables.pl').
+:- dynamic of/2.
+:- dynamic mf/2.
+:- dynamic maxOF/1, minOF/1.
+:- dynamic maxMF/1, minMF/1.
+
+carbonScores() :-
+    retractall(cs(_,_)), 
+    nodeRankingFactors(),
+    findall(1, carbonScore(M,C), Cs), sum_list(Cs,N), writeln(N),
+    cleanUp().
+
+/* Computes and asserts the carbon score for each Node into facts of the form 
+        cs(Node, CarbonScore).
+    where 
+        CarbonScore is 0.5 * (OF-MinOF)/(MaxOF-MinOF) + 0.5 * (MF-MinMF)/(MaxMF-MinMF) 
+*/
+carbonScore(N,CS) :- 
+    node(N,_,_,_,_,_),
+    of(N,OF), minOF(MinOF), maxOF(MaxOF),
+    P1 is 0.5 * (OF-MinOF)/(MaxOF-MinOF),
+    mf(N,MF), minMF(MinMF), maxMF(MaxMF),
+    P2 is 0.5 * (MF-MinMF)/(MaxMF-MinMF),
+    CS is P1 + P2, assert(cs(N,CS)).
+
+nodeRankingFactors() :-
+    findall(OF, nodeOF(N,OF), OFs), max_list(OFs, MaxOF), min_list(OFs,MinOF),
+    assert(maxOF(MaxOF)), assert(minOF(MinOF)),
+    findall(MF, nodeMF(N,MF), MFs), max_list(MFs,MaxMF), min_list(MFs,MinMF),
+    assert(maxMF(MaxMF)), assert(minMF(MinMF)).
+
+cleanUp() :-
+    retractall(of(_,_)), retractall(maxOF(_)), retractall(minOF(_)),
+    retractall(mf(_,_)), retractall(maxMF(_)), retractall(minMF(_)).
+
+nodeOF(N,OF) :- node(N,_,PowerPerCPU,_,_,PUE), carbon_intensity(N,I), OF is PUE * I * PowerPerCPU, assert(of(N,OF)).
+
+nodeMF(N,MF) :- node(N,_,_,EL,TE,_), MF is TE/EL, assert(mf(N,MF)).
+
+%# Finds the placement with the lowest SCI. In case of a tie in SCI, returns the placement that uses the fewest nodes.
+minPlacement(App, P, SCI, NumberOfNodes) :-
+    placement(App, P, SCI, NumberOfNodes),
+    \+ (placement(App, P1, S1, N1), dif(P1,P),  (S1 < SCI ; (S1 =:= SCI, N1 < NumberOfNodes))).
+    
+%# Finds a valid placement for the application and returns the SCI and the number of nodes associated with the placement.
+placement(App, P, SCI, NumberOfNodes) :-
+    application(App, Ms, EPs),
+    functionalUnits(App, R),
+    eligiblePlacement(Ms, P), 
+    involvedNodes(P, NumberOfNodes),
+    sci(EPs, R, P, SCI).
+
+eligiblePlacement(Ms, P) :- eligiblePlacement(Ms, [], P).
+%# Finds a valid placement for the list of microservices.
+eligiblePlacement([M|Ms], P, NewP) :-
+    microservice(M, RR, _),
+    placementNode(N, P, RR),
+    eligiblePlacement(Ms, [on(M,N)|P], NewP).
+eligiblePlacement([], P, P).
+
+%# Checks if the node N can host the microservice M.
+placementNode(N, P, rr(CPUReq, RAMReq, BWinReq, BWoutReq)) :-
+    node(N, tor(CPU, RAM, BWin, BWout), _, _, _, _),
+    hardwareUsedAtNode(N, P, rr(UCPU, URAM, UBWin, UBWout)),
+    CPU >= UCPU + CPUReq, 
+    RAM >= URAM + RAMReq, 
+    BWin >= UBWin + BWinReq, 
+    BWout >= UBWout + BWoutReq.
+
+%# Counts the number of nodes used by the placement.
+involvedNodes(P, InvolvedNodes) :-
+    findall(N, distinct(node(N,_), member(on(_,N),P)), Nodes), 
+    length(Nodes, InvolvedNodes).
+
+%# Calculates the amount of hardware used on node N.
+hardwareUsedAtNode(N, P, rr(UCPU, URAM, UBWin, UBWout)) :-
+    findall(rr(CPU,RAM,BWin,BWout), (member(on(M,N),P), microservice(M,rr(CPU,RAM,BWin,BWout),_)), RRs),
+    sumHWReqs(RRs, rr(UCPU, URAM, UBWin, UBWout)).
+
+%# Sums the hardware requirements of each microservice M placed on node N.
+sumHWReqs([rr(CPU,RAM,BWin,BWout) | RRs], rr(TCPU, TRAM, TBWin, TBWout)) :-
+    sumHWReqs(RRs, rr(AccCPU, AccRAM, AccBWin, AccBWout)),
+    TCPU is AccCPU + CPU,
+    TRAM is AccRAM + RAM,
+    TBWin is AccBWin + BWin,
+    TBWout is AccBWout + BWout.
+sumHWReqs([], rr(0,0,0,0)).
+
+%# Calculates the SCI of the application's placement.
+sci(EndPoints, R, P, SCI) :- sci(EndPoints,R,P,0,SCI).
+sci([EP|EPs], R, P, OldSCI, NewSCI) :-
+    endpointSCI(EP,R,P,EPSCI),
+    TmpSCI is OldSCI + EPSCI,
+    sci(EPs,R,P,TmpSCI,NewSCI).
+sci([],_,_,SCI,SCI).
+
+%# Calculates the SCI relative to a single endpoint considering the probability
+%# that said endpoint is called.
+endpointSCI(EP, R, P, SCI) :-
+    endpoint(EP, EPMs),
+    findall(on(M,N), (member(M, EPMs), member(on(M, N), P)), FilteredP),
+    probability(EP, Prob),
+    carbonEmissions(FilteredP, C),
+    SCI is (C / R) * Prob.
+
+%# Calculates the carbon amount of a placement.
+carbonEmissions([on(Microservice,Node)|P], C) :-
+    carbonEmissions(P, AccC),
+    operationalCarbon(Node, Microservice, O),
+    embodiedCarbon(Node, Microservice, E),
+    C is AccC + O + E.
+carbonEmissions([], 0).
+
+%# Calculates the amount of energy required to run microservice M on node N for the entire TiL.
+operationalEnergy(Node, Microservice, E) :-
+    node(Node, _, PowerPerCPU, _, _, PUE),
+    microservice(Microservice, _, TiR),
+    E is PUE * (TiR * 365 * 24) * PowerPerCPU.
+
+%# Calculates the carbon intensity of running microservice M on node N.
+operationalCarbon(Node, Microservice, O) :-
+    carbon_intensity(Node, I),
+    operationalEnergy(Node, Microservice, E),
+    O is E * I.
+
+%# Calculates the embodied carbon intensity of microservice M on node N.
+embodiedCarbon(Node, Microservice, M) :-
+    node(Node, tor(CPU,_,_,_), _, EL, TE, _),
+    microservice(Microservice, rr(CPUReq,_,_,_), TiR),
+    TS is TiR / EL,
+    RS is CPUReq / CPU,
+    M is TE * TS * RS.
+
